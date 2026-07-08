be13358129772720664576dc3d3e25105dc90ed1
EllieK-coder <160268589+EllieK-coder@users.noreply.github.com>
2026-07-08 20:09:56 +0300

    Add files via upload

---
+++ WorldRank/IPlayer.cs
@@ -0,0 +1,7 @@
+﻿public interface IPlayer
+{
+    protected int Id { get; set; }
+    protected string Name { get; set; }
+    protected int Score { get; set; }
+}
+
---
+++ WorldRank/IWalletRepository.cs
@@ -0,0 +1,7 @@
+﻿using WorldRank;
+
+public interface IWalletRepository
+{
+	void Add(Wallet wallet, int playerId);
+	List<Wallet> GetByPlayerId(int playerId);
+}
